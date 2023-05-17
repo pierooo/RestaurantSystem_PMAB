5bff2d365f8c5ec79730240b64a8c47c5e07ba23
Piotr Utrata <piotrek.utrata@gmail.com>
2023-05-17 15:21:16 +0200

    Initial Xamarin project

---
+++ RestaurantSystem/RestaurantSystem/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using RestaurantSystem.Services;
+using RestaurantSystem.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RestaurantSystem
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ RestaurantSystem/RestaurantSystem/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using RestaurantSystem.Models;
+using RestaurantSystem.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace RestaurantSystem.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
